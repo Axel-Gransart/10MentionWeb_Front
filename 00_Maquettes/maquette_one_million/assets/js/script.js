d9536c90fe5563cc4adef71f73ad91d0c55eb980
rocket <gransart.axel@gmail.com>
2024-05-14 16:36:50 +0200

    modif maquette one million

--- 00_Maquettes/maquette_one_million/assets/js/script.js
+++ 00_Maquettes/maquette_one_million/assets/js/script.js
@@ -1,32 +1,58 @@
 
 let teamMembers = document.querySelectorAll('.team h4');
 
-console.log(teamMembers.textContent);
+
+// console.log(teamMembers.textContent);
+
+// for (let member of teamMembers) {
+//   console.log(member.textContent);
+//   let memberDiv = member.parentElement;
+//   let post = memberDiv.querySelector('p');
+
+//   memberDiv.addEventListener('mouseover', ()=> {   
+//     post.style.color = 'white';
+    
+//     if (member.textContent.includes('joanne')) {
+//       memberDiv.style.backgroundImage = "url('assets/images/joanne.jpg')";
+//     }
+//     else  if (member.textContent.includes('gaston')) {
+//       memberDiv.style.backgroundImage = "url('assets/images/gaston.jpg')";
+//     }
+//     else  if (member.textContent.includes('manuel')) {
+//       memberDiv.style.backgroundImage = "url('assets/images/manuel.jpg')";
+//     }
+//     else  if (member.textContent.includes('tracy')) {
+//       memberDiv.style.backgroundImage = "url('assets/images/tracy.jpg')";
+//     }
+//   })
+//   memberDiv.addEventListener('mouseleave', ()=> {
+//     memberDiv.style.backgroundImage = "";
+//     post.style.color = 'black';
+//   })
+// }
+
 
 for (let member of teamMembers) {
-  console.log(member.textContent);
   let memberDiv = member.parentElement;
-  memberDiv.style.backgroundSize = 'cover';
   let post = memberDiv.querySelector('p');
+  
+  function background(members) {
+    if (member.textContent.includes(members)) {
+      memberDiv.style.backgroundImage = `url('assets/images/${members}.jpg')`;
+    }
+  }
 
-  memberDiv.addEventListener('mouseover', ()=> {   
+  memberDiv.addEventListener('mouseenter', ()=> {
     post.style.color = 'white';
-    
-    if (member.textContent.includes('joanne')) {
-      memberDiv.style.backgroundImage = "url('assets/images/joanne.jpg')";
-    }
-    else  if (member.textContent.includes('gaston')) {
-      memberDiv.style.backgroundImage = "url('assets/images/gaston.jpg')";
-    }
-    else  if (member.textContent.includes('manuel')) {
-      memberDiv.style.backgroundImage = "url('assets/images/manuel.jpg')";
-    }
-    else  if (member.textContent.includes('tracy')) {
-      memberDiv.style.backgroundImage = "url('assets/images/tracy.jpg')";
-    }
+    background('joanne');
+    background('gaston');
+    background('manuel');
+    background('tracy');
   })
-  memberDiv.addEventListener('mouseout', ()=> {
+
+  memberDiv.addEventListener('mouseleave', ()=> {
     memberDiv.style.backgroundImage = "";
     post.style.color = 'black';
   })
 }
+
